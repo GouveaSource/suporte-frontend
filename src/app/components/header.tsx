c73fab5d58d6a8a9754c4f57f22138b426fdf296
Raul Gouvea <raul.gouveacco@grupocarvalhogestao.com.br>
2025-08-08 15:49:34 -0300

    feat: Adição do componente Header e configuração de estilos

---
+++ src/app/components/header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Header = () => {
+    return (
+        <nav className='navbar navbar-expand-1g navbar-dark fixed-top'>
+            <div className='container'>
+
+            </div>
+
+        </nav>
+    );
+};
+
+export default Header;
